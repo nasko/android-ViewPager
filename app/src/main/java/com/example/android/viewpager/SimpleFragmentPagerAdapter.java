6b07962f65b4bee274fa6894f23dc1666555c54f
Atanas Vasilev <atanas.vasilev@gmail.com>
2017-07-25 01:06:08 +0300

    Add two additional fragments

--- app/src/main/java/com/example/android/viewpager/SimpleFragmentPagerAdapter.java
+++ app/src/main/java/com/example/android/viewpager/SimpleFragmentPagerAdapter.java
@@ -18,29 +18,50 @@ package com.example.android.viewpager;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.util.ArrayMap;
+
+import java.util.ArrayList;
 
 /**
  * Provides the appropriate {@link Fragment} for a view pager.
  */
 public class SimpleFragmentPagerAdapter extends FragmentPagerAdapter {
 
+    private ArrayMap<String, String> fragments = new ArrayMap<String, String>();
+
     public SimpleFragmentPagerAdapter(FragmentManager fm) {
         super(fm);
+
+        fragments.put("0", "MondayFragment");
+        fragments.put("1", "TuesdayFragment");
+        fragments.put("2", "WednesdayFragment");
+        fragments.put("3", "ThursdayFragment");
+        fragments.put("4", "FridayFragment");
     }
 
     @Override
     public Fragment getItem(int position) {
-        if (position == 0) {
-            return new MondayFragment();
-        } else if (position == 1){
-            return new TuesdayFragment();
-        } else {
-            return new WednesdayFragment();
+        Class<?> clazz = null;
+        try {
+            clazz = Class.forName("com.example.android.viewpager."
+                + fragments.get(String.valueOf(position)));
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
         }
+
+        Fragment fr = null;
+        try {
+            fr = (Fragment) clazz.newInstance();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return fr;
     }
 
     @Override
     public int getCount() {
-        return 3;
+        return fragments.size();
     }
 }
